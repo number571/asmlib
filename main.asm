3af884e90c69d668097790cbfae2e0871aad7a49
[#571] <31502293+Number571@users.noreply.github.com>
2019-08-07 14:17:22 -0500

    Update main.asm

--- main.asm
+++ main.asm
@@ -2,24 +2,13 @@ format ELF64
 public _start
 
 include "asmlib/fmt.inc"
+include "asmlib/mth.inc"
 include "asmlib/sys.inc"
-
-section '.data' writable
-	fmt db "[%d + %d = %d]", 0xA, 0
 
 section '.text' executable 
 _start:
-	mov rbx, 100
-	mov rcx, 50
-
-	mov rdx, rbx
-	add rdx, rcx
-
-	push rdx
-	push rbx
-	push rcx
-
-	mov rax, fmt
-
-	call printf
-	call exit
+    mov rax, 6
+    call factorial
+    call print_number
+    call print_line
+    call exit
