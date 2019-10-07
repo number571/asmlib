b83256319cd39c9de153ac52c6c58d98c56e52a0
[#571] <31502293+Number571@users.noreply.github.com>
2019-10-07 17:18:51 -0500

    Update main.asm

--- main.asm
+++ main.asm
@@ -5,10 +5,53 @@ include "asmlib/fmt.inc"
 include "asmlib/mth.inc"
 include "asmlib/sys.inc"
 
-section '.text' executable 
+section '.bss' writeable
+    input_size equ 1024
+    input rb input_size
+
+section '.data' writeable
+    quit db ":q", 0
+    console db ">> ", 0
+
+section '.text' executable
 _start:
-    mov rax, 6
-    call factorial
-    call print_number
-    call print_line
-    call exit
+    .next_iter:
+        mov rax, console
+        call print_string
+
+        mov rax, input
+        mov rbx, input_size
+        call input_string
+
+        push rax
+        call readstring
+        cmp rax, 0
+        je .close
+        pop rax
+
+        call rpn_interpret
+        call print_integer
+        call print_line
+        jmp .next_iter
+
+    .close:
+        call exit
+
+section '.readstring' executable
+; | input:
+; rax = string
+; | output:
+; rax = number
+readstring:
+    push rcx
+    push rsi
+    push rdi 
+    mov rsi, rax
+    mov rdi, quit
+    mov rcx, 3
+    repe cmpsb
+    mov rax, rcx
+    pop rdi
+    pop rsi
+    pop rcx
+    ret
