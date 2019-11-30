d0e5cdd3e95d33f9fe87a1009eaf36c828ad927f
[#571] <31502293+Number571@users.noreply.github.com>
2019-11-30 12:35:16 -0500

    Update inv-num.asm

--- _example/inv-num.asm
+++ _example/inv-num.asm
@@ -6,21 +6,20 @@ include "asmlib/mth.inc"
 include "asmlib/sys.inc"
 
 section '.data' writeable
-	x equ 3
+	x equ 5
 	n equ 26
 
 ; x^(f(n)-1) mod n = x^-1
 section '.text' executable
 _start:
 	mov rax, n
-	dec rax
 	call euler
+	dec rax
 	push rax
 	mov rax, x
 	pop rbx
-	call pow
-	mov rbx, n
-	call modulo
+	mov rcx, n
+	call powmod
 	call print_number
 	call print_line
 	jmp exit
